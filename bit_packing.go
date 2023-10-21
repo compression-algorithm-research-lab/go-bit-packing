54a7c7d907cc00124a0d4bbc5e35d052aae93754
cc11001100 <cc11001100@qq.com>
2023-10-21 11:44:55 +0800

    init

---
+++ bit_packing.go
@@ -0,0 +1,25 @@
+package bit_packing
+
+import "github.com/golang-infrastructure/go-gtypes"
+
+// EncodeBoolSlice 把bool数组编码为字节数组
+func EncodeBoolSlice(boolSlice []bool) []byte {
+
+}
+
+func DecodeBoolSlice(byteSlice []byte) []bool {
+
+}
+
+// ------------------------------------------------ ---------------------------------------------------------------------
+
+// EncodeIntSliceToBytes 把int类型的数组编码为字节数组
+func EncodeIntSliceToBytes[T gtypes.Unsigned](slice []T, byteLength int) []byte {
+	
+}
+
+func DecodeIntSliceFromBytes() {
+
+}
+
+// ------------------------------------------------ ---------------------------------------------------------------------
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/compression-algorithm-research-lab/go-bit-packing
+
+go 1.19
+
+require github.com/golang-infrastructure/go-gtypes v0.0.1 // indirect
